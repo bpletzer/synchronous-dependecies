591edd507897f0a3022aa2b8b5713285770dde7b
bpletzer <bpletzer@googlemail.com>
2012-06-14 16:44:52 +0200

    ibla

--- js/syncQueue.js
+++ js/syncQueue.js
@@ -2,21 +2,27 @@
 	root.sQ = factory();
 	d.write('<script src="data:text/javascript;base64,c1EuZHU9MQ=="><'+'/script>');
 
+	if ("function" typeof define && define.amd) {
+		define("syncQueue", root.sQ);
+	}
+
 })(this, document, function(){
 	var queue = [],
 		triggerSyncDequeue = (this.du) ? "data:text/javascript;base64,c1EuZHEoKQ==" : "js/triggerSyncDequeue.js";
 
 	return {
-		enqueue: function  (src, func){
+		load: function  (src, func){
 			queue.push(func);
 			document.write('<script src="'+src+'"><'+'/script>', '<script src="'+triggerSyncDequeue+'"><'+'/script>');
 		},
+		// dequeue
 		dq: function () {
 			var item = queue.shift();
 			if (item) {
 				item.call()
 			}
 		},
+		// du: is script dataUri possible
 		du: false
 	};
 });
